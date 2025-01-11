4500340378ca30369bf35dda5c6efefb802e1cd0
Harman-DevCloud <hmn30004@gmail.com>
2025-01-11 09:54:07 -0700

    Is Prime Solution

---
+++ Basics/isPrime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class isPrime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+
+        int count = 0;
+        for(int div = 2; div*div < num; div++ ){
+            if(num % div == 0){
+                count++;
+                break;
+            }
+            div++;
+        }
+        if(count == 0){
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is not Prime");
+        }
+    }
+}
