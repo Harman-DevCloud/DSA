660bcc8551c82b5b31335cec3d3071fcae94c08e
Harman-DevCloud <hmn30004@gmail.com>
2025-01-12 10:03:46 -0700

    Count digit,fib,printdigit Solution

---
+++ Basics/fibUptoN.java
@@ -0,0 +1,21 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class fibUptoN {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int c = a+b;
+
+        for (int i = 1; i <= n ; i++) {
+            System.out.println(a);
+            a = b;
+            b = c;
+            c = a+b;
+        }
+
+    }
+}
